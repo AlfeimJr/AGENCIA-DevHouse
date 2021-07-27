e1cf00aa188aa33485b3bdf22bdbd766a5af1a46
Gabriel Renovato <gabriel_renovato@outlook.com>
2021-07-27 20:44:22 -0300

    Alterações

--- app.js
+++ app.js
@@ -2,14 +2,32 @@
 const express = require('express')
 
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
 
 app.get('/', (req, res)=>{
-    res.send('Hello World')
+    res.sendFile(__dirname + '/views/home.html')
 })
+
+app.get('/home', (req, res)=>{
+    res.sendFile(__dirname + '/views/home.html')})
 
 app.get('/manutencao', (req, res)=>{
     res.sendFile(__dirname + '/views/manutencao.html')})
 
+    app.get('/blog', (req, res)=>{
+        res.sendFile(__dirname + '/views/blog.html')})
+
+
+app.get('/contato', (req, res)=>{
+    res.sendFile(__dirname + '/views/contato.html')})
+
+app.post('/receber-contato', (req,res)=> {
+    console.log(req.body);
+    res.send('contato recebido')  
+})
 
 app.listen(3000, () => console.log('servidor online'))
 
