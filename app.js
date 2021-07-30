ed3fc8cfede103965c355acb96ba065efeff06f2
Gabriel Renovato <gabriel_renovato@outlook.com>
2021-07-29 22:12:31 -0300

    Alterações dia 29/07/2021

--- app.js
+++ app.js
@@ -1,33 +1,15 @@
 //Criar o servidor
 const express = require('express')
+const rotas = require('./routes')
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
+app.use(rotas)
 
 
-app.get('/', (req, res)=>{
-    res.sendFile(__dirname + '/views/home.html')
-})
 
-app.get('/home', (req, res)=>{
-    res.sendFile(__dirname + '/views/home.html')})
-
-app.get('/manutencao', (req, res)=>{
-    res.sendFile(__dirname + '/views/manutencao.html')})
-
-    app.get('/blog', (req, res)=>{
-        res.sendFile(__dirname + '/views/blog.html')})
-
-
-app.get('/contato', (req, res)=>{
-    res.sendFile(__dirname + '/views/contato.html')})
-
-app.post('/receber-contato', (req,res)=> {
-    console.log(req.body);
-    res.send('contato recebido')  
-})
 
 app.listen(3000, () => console.log('servidor online'))
 
